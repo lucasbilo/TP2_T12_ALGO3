d5d88a304fef95ebc80f956439e58f4f4b107585
lucasbilo <lbilo@fi.uba.ar>
2021-02-02 20:52:54 -0300

    Se agregan los metodos obtenerPosicion, existePosicion y actualizar.

--- src/main/java/edu/fiuba/algo3/modelo/Tablero.java
+++ src/main/java/edu/fiuba/algo3/modelo/Tablero.java
@@ -1,14 +1,38 @@
 package edu.fiuba.algo3.modelo;
 
 import java.util.ArrayList;
+import java.util.Optional;
 
 public class Tablero {
 
     private ArrayList<Posicion> posiciones = new ArrayList<Posicion>();
 
-    public void escribir(Posicion posicion){
+    public Tablero(){}
 
-        posiciones.add(posicion);
+    public void escribir(Posicion posicion){
+        Posicion nuevaPosicion = this.obtenerPosicion(posicion.obtenerX(), posicion.obtenerY());
+        if(nuevaPosicion == null) { //si entra al if significa que la posicion no esta cargada en el tablero
+            this.posiciones.add(posicion);
+        }
+    }
+
+    public Posicion obtenerPosicion(int x, int y){
+        Optional<Posicion> buscada = this.posiciones.stream()
+                .filter(posicion -> (posicion.obtenerX() == x)
+                        && (posicion.obtenerY() == y))
+                .findFirst();
+        return buscada.orElse(null); //devuelve NULL si no se encuentra
+    }
+
+    public boolean existePosicion(int x, int y){
+        return this.posiciones.stream()
+                .anyMatch(posicion -> (posicion.obtenerX() == x) && (posicion.obtenerY() == y));
+    }
+
+    public void actualizar(Posicion posicion){
+        Posicion posicionNueva = new Posicion(posicion.obtenerX(), posicion.obtenerY());
+        posicionNueva.modificarEstadoLapiz(posicion.obtenerEstadoLapiz());
+        this.escribir(posicionNueva);
     }
 
 }
